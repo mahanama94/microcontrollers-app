3deae9a0cd0e2f78066cf8c18f0584070a77052a
mahanama94 <bhanuka.14@cse.mrt.ac.lk>
2017-07-08 14:42:51 +0530

    View improvements Added refresh for home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NavController, LoadingController} from 'ionic-angular';
+import {NavController, LoadingController, Events} from 'ionic-angular';
 import {RemoteService} from "../../Services/RemoteService";
 
 @Component({
@@ -14,19 +14,24 @@ export class HomePage implements OnInit{
       let loader = this.loadingController.create({
         content : "Loading... Please wait.."
       });
-      loader.present();
+      //loader.present();
       this.remoteService.getRemotes()
         .then((data : Array<String>) =>{
           this.items = data;
           loader.dismissAll();
-      }).catch((error) => {alert(JSON.stringify(error)); loader.dismissAll()});
+      }).catch((error) => {
+        //alert(JSON.stringify(error));
+        loader.dismissAll()
+      });
   }
 
-  constructor(public navCtrl: NavController, private remoteService: RemoteService, private loadingController: LoadingController) {
-
+  constructor(public navCtrl: NavController, private remoteService: RemoteService, private loadingController: LoadingController, public events: Events) {
+    this.events.subscribe('HomeSelect', () => {this.ngOnInit() })
   }
 
   open(item: any){
     alert("Selected ");
   }
+
+
 }
